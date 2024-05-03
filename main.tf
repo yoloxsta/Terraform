f6321b1dabbf83939e05fb9f999e5f1474a7f14b
soetintaung <soetintaunghz10@gmail.com>
2024-05-03 10:03:56 +0630

    lab

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-06c4be2792f419b7b"      #change ami id for different region
+  instance_type          = "t2.large"
+  key_name               = "sta-key"              #change key name as per your setup
+  vpc_security_group_ids = [aws_security_group.Jenkins-VM-SG.id]
+  subnet_id              = "subnet-0175c6f1dd3d2b02e" #change your subnet to be lauch zone
+  user_data              = templatefile("./install.sh", {})
+
+  tags = {
+    Name = "Jenkins-SonarQube"
+  }
+
+  root_block_device {
+    volume_size = 40
+  }
+}
+
+resource "aws_security_group" "Jenkins-VM-SG" {
+  name        = "Jenkins-VM-SG"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-02e548d0256bac144" #your vpc-id
+
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000] : {
+      description      = "inbound rules"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jenkins-VM-SG"
+  }
+}
